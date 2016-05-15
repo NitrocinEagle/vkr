53793c815d31f377f62c2df7e5c6724984eaeab2
chepapka <4815162342llgs@gmail.com>
2016-05-15 16:38:48 +0700

    add table theory

--- vkr/models.py
+++ vkr/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class theory(models.Model):
+    class Meta():
+        db_table = "theory"
+    theory_title = models.CharField(max_length = 200)
+    theory_text = models.TextField()
+    theory_date = models.DateField()
