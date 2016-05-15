5ee9111ff5fe310a5489b7c41bc3e2a864bd9acf
chepapka <4815162342llgs@gmail.com>
2016-05-15 17:48:32 +0700

    add start page and main page

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^main/', 'vkr.views.main'),
+    url(r'^', 'vkr.views.index'),
 ]
--- vkr/views.py
+++ vkr/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http.response import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
 # Create your views here.
+
+def main(request):
+    view = "main"
+    html = "<html><body> This is %s view</body></html>" % view
+    return HttpResponse(html)
+
+def index(request):
+    view = 'SFU'
+    t = get_template('index.html')
+    html = t.render(Context({'name': view}))
+    return HttpResponse(html)
