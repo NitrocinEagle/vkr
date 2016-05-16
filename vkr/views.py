91d9046a2b4f20396d21ceb0fa8388d643803626
chepapka <4815162342llgs@gmail.com>
2016-05-16 15:19:36 +0700

    add Bootstrap framework, add new app loginsys

--- vkr/views.py
+++ vkr/views.py
@@ -4,16 +4,16 @@ from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response
 from vkr.models import theory
-
+from django.contrib import auth
 
 # Create your views here.
 
 def index(request):
-    return render_to_response('allTheory.html', {'allTheory': theory.objects.all()})
+    return render_to_response('allTheory.html', {'allTheory': theory.objects.all(), 'username': auth.get_user(request).username})
 
-def allTheory(request):
-    return render_to_response('allTheory.html', {'allTheory': theory.objects.all()})
+#def allTheory(request):
+    #return render_to_response('allTheory.html', {'allTheory': theory.objects.all()})
 
 
 def oneTheory(request, theory_id):
-    return render_to_response('oneTheory.html', {'oneTheory': theory.objects.get(id=theory_id)})
+    return render_to_response('oneTheory.html', {'oneTheory': theory.objects.get(id=theory_id), 'username': auth.get_user(request).username})
